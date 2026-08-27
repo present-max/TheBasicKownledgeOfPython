2248a1d693854631a3aa58415c67a03cd20be360
present <2641362143@qq.com>
2026-08-27 12:19:58 +0800

    __all__,__name__的使用

--- FirstUnit/function.py
+++ FirstUnit/function.py
@@ -1,3 +1,5 @@
+# 指定模块的导出内容
+__all__ = ["stu","caculate","cube","function"]
 #默认参数要放在最后
 def stu(name,score,gender,age=19):
     #函数的说明文档
@@ -30,6 +32,12 @@ num=3
 def function(cube,num):
     return cube(num)
 print(function(cube,num))
+
+# 当模块被直接运行时，__name__为__main__，当模块被导入时，__name__为模块名，所以这里判断是否是被直接运行
+if __name__ == '__main__':
+    stu("zhangsan",90,"male")
+    minnum,maxnum,avgnum=caculate(1,2,3,4,5,6,7,8,9,10,round=2)
+    print(minnum,maxnum,avgnum)
 
 
 
