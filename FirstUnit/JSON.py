bf965a3f3557c53c026ba782345d472176b9972b
present <2641362143@qq.com>
2026-08-28 18:37:44 +0800

    Json的操作补充

--- FirstUnit/JSON.py
+++ FirstUnit/JSON.py
@@ -16,7 +16,8 @@ print(user_dict)
 
 #将字典转换为JSON字符串并保存到文件
 with open("user.json", "w", encoding="utf-8") as f:
-    json.dump(user, f)
+    # ensure_ascii=False: 确保非ASCII字符(若为true，中文就会被转义，不会显示原文字)不被转义，indent=2: 确保JSON字符串格式化输出
+    json.dump(user, f, ensure_ascii=False, indent=2)
 print("JSON数据已保存到user.json文件中")
 #从文件中读取JSON数据并转换为字典
 with open("user.json", "r", encoding="utf-8") as f:
